530ce9756817867310e2afaac61dfcdb73aba71a
farheenfathimaa <farheennfathima@gmail.com>
2023-09-10 15:56:12 +0530

    band name generator

---
+++ day1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city=input("enter name of the city you live in: ")
+name=input("enter your pet's name: ")
+print("Your brand name could be: "+city+" "+name)
