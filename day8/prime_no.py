58bce1398cf0c24d0be84c10e0a5927fb78ea0cd
farheenfathimaa <farheennfathima@gmail.com>
2023-09-18 23:08:58 +0530

    update prime_no.py

--- day8/prime_no.py
+++ day8/prime_no.py
@@ -1,9 +1,11 @@
 n=int(input("Enter a number: "))
-count=0
-for i in range(1,n+1):
-    if n%i==0:
-        count+=1
-if count==2:
-    print("It's a prime number") 
-else:
-    print("It's not a prime number") 
+def prime_checker(n):
+    count=0
+    for i in range(1,n+1):
+        if n%i==0:
+            count+=1
+    if count==2:
+        print("It's a prime number") 
+    else:
+        print("It's not a prime number") 
+prime_checker(n)
