3bc1090ad62dea711ed1e89809a967d8717b1359
Farheen Fathima <farheennfathima@gmail.com>
2024-01-12 00:35:15 +0530

    US states guessing game

--- day25/main.py
+++ day25/main.py
@@ -14,6 +14,15 @@ guessed_states=[]
 while len(guessed_states)<50:
     answer_state=screen.textinput(title=f"({len(guessed_states)}/50) States guessed", prompt="What's another state's name?").title()
     if answer_state=="Exit":
+        # with list comprehension
+        remaining_states=[s for s in all_states if s not in guessed_states]
+        # without list comprehension
+        # remaining_states=[]
+        # for s in all_states:
+        #     if s not in guessed_states:
+        #         remaining_states.append(s)
+        df=pandas.DataFrame(remaining_states)
+        df.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -23,10 +32,5 @@ while len(guessed_states)<50:
         state_data=states[states.state==answer_state]
         t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
         t.write(answer_state)
-remaining_states=[]
-for s in all_states:
-    if s not in guessed_states:
-        remaining_states.append(s)
-df=pandas.DataFrame(remaining_states)
-df.to_csv("states_to_learn.csv")
+
 
