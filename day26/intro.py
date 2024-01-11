ee4565f0fa2bae38c6b36a10222423a14de88b12
Farheen Fathima <farheennfathima@gmail.com>
2024-01-12 01:27:07 +0530

    day 26

--- day26/intro.py
+++ day26/intro.py
@@ -20,3 +20,22 @@ student_scores={name:random.randint(1,100) for name in names}
 passed_students={name:score for (name,score) in student_scores.items() if score>60}
 print(student_scores)
 print(passed_students)
+
+# to iterate over pandas dataframes
+import pandas 
+student_dict={
+    "student":["angela","james","lily"],
+    "score":[56,76,98]
+}
+#looping through dictionaries:
+#for (key:value) in student_dict.items():
+#   print(value)
+
+df=pandas.DataFrame(student_dict)
+print(df)
+#iterating through rows of a dataframe
+for index,row in df.iterrows():
+    #index is the row number and row contains the actual data
+    print(index)
+    print(row)
+    print(row.score)
