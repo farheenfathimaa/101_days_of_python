b4c4e5232403b98934864269b7d09a43471db28d
Farheen Fathima <farheennfathima@gmail.com>
2024-01-13 11:19:51 +0530

    catching errors

--- day26/main.py
+++ day26/main.py
@@ -7,6 +7,15 @@ student_data_frame = pandas.DataFrame(alphabets)
 phonetic_dict={row.letter:row.code for (index, row) in student_data_frame.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word=input("Enter a word: ").upper()
-output_list=[phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generating_phonetic():
+    word=input("Enter a word: ").upper()
+    try:
+        output_list=[phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Invalid input! Please enter a word.")
+        generating_phonetic()
+    else:
+        print(output_list)
+
+generating_phonetic()
