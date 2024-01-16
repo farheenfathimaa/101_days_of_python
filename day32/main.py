e931ec778e1b1aecef8e60087c2e45544c64e640
Farheen Fathima <farheennfathima@gmail.com>
2024-01-16 20:05:49 +0530

    monday motivation

---
+++ day32/main.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import random
+import smtplib
+
+now=dt.datetime.now()
+week_day=now.weekday()
+
+
+with open("quotes.txt") as quotes:
+    lines = quotes.readlines()
+    
+quote=random.choice(lines)
+
+my_email="somethingisfishy93@gmail.com"
+password="dlwnxjxpiqcvmnly"
+
+if week_day==0:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()  
+        connection.login(user=my_email, password=password)
+        subject="Today's Quote!"
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="somethingisfishy93@yahoo.com",
+                            msg=f"Subject:{subject}\n\n{quote}")
+else:
+    print("it's not Monday today!")
