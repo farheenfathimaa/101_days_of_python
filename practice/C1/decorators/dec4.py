54c55e2eebf6078525feb4631cb453fa3de152d7
Farheen Fathima <farheennfathima@gmail.com>
2024-10-22 16:41:47 +0530

    .

---
+++ practice/C1/decorators/dec4.py
@@ -0,0 +1,8 @@
+# 4. Counting Function Calls
+# Problem: Create a decorator that counts how many times 
+# a function has been called. Use it on a function that 
+# returns a greeting message.
+
+# Expected Output:
+# greet() has been called 1 times
+# greet() has been called 2 times
---
+++ practice/C1/decorators/dec5.py
@@ -0,0 +1,12 @@
+# 5. Cashing Function Results
+# Problem: Implement a decorator that caches the results 
+# of a function call to avoid recomputation if the same 
+# arguments are passed again. Test it with a function that
+# squares a number.
+
+# Expected Output:
+# Computing square(4)
+# Result: 16
+# Using cached result for square(4)
+# Result: 16
+
